b0b1805c76450cdcc809ec1bd98318620ce712f4
rmit-s3330189-billy-vasiliadis <billvas93@gmail.com>
2018-03-31 19:17:29 +1100

    dbquery program, take argument and check if it matches a business name, if it does, print the record to the console

--- dbquery.java
+++ dbquery.java
@@ -1,25 +1,38 @@
 import java.io.*;
 
 public class dbquery {
+	private static String bn_name;	
+
 	public static void main(String[] args) {
 		FileReader input = null;
+		bn_name = "(.*)" + args[0].toLowerCase() + "(.*)";
+		String datafile = "heap." + args[args.length - 1] + ".dat";
 
 		try{
-			input = new FileReader("result.dat");
+			input = new FileReader(datafile);
 			String record = "";			
 			int c = 0;
 			while(c != -1) {
 				while((c = input.read()) != 37 && c != -1 && c != 10) {
 					record += (char)c;
 				} 
-				System.out.println(record);
+				if(record != "" && checkBnName(record)) {
+					System.out.println(record);
+				}
 				record="";
 			}
 		} catch(Exception e){
 			System.out.println(e);
 		}
-
 	}
-
-
+	
+	public static boolean checkBnName(String record) {
+		String[] temp = record.split(",");
+		String name = temp[1].toLowerCase();
+		if(name.matches(bn_name)) {
+			System.out.println("Record found: ");
+			return true;
+		}
+		return false;
+	}
 }
